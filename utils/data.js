4c822c5a6cdc3f44fc6f98e678bfd2598710443d
jennifer arevalo <arevalo5007@gmail.com>
2023-09-21 18:25:00 -0500

    add to seed data

--- utils/data.js
+++ utils/data.js
@@ -2,14 +2,48 @@ const userdata = [
     {
         username: 'iluvcats',
         email: 'luvcats@example.com'
+    },
+    {
+        username:'jenn88',
+        email:'jenn88@yahoo.com'
+    },
+    {
+        username:'skynet',
+        email: '123sky@gmail.com'
+    },
+    {
+        username:'solo',
+        email: 'hansolo@aol.com'
+    },
+    {
+        username: 'niki00',
+        email: 'nicoleA@aol.com'
     }
-]
+];
 
 const thoughtdata = [
     {
         username: 'iluvcats',
         thoughtText: 'love cats!'
+    },
+    {
+        username:'jenn88',
+        thoughtText: 'hey hows everybody? im doing good lol'
+
+    },
+    {
+        username:'skynet',
+        thoughtText: 'im just a regular human'
+    },
+    {
+        username: 'solo',
+        thoughtText: 'I did the cesle run in 12 parcects'
+    
+    },
+    {
+        username:'niki00',
+        thoughtText: 'i cant wait for it to get cold!'
     }
-]
+];
 
 module.exports = { userdata, thoughtdata}
